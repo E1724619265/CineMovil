ef74b01337c9416029e92e6aaea87101b787547f
Cristian Morocho <Cristian Morocho@CMOROCHO>
2023-01-16 12:14:23 -0500

    Agregar archivos de proyecto.

---
+++ CineMovil/Vistas/login.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CineMovil.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btningresa_Clicked(object sender, EventArgs e)
+        {
+            string user = "admin";
+            string pass = "Admin001";
+
+            if (user == txtusser.Text && pass == txtpassword.Text)
+            {
+                Navigation.PushAsync(new crecomendado(user));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario y/o Contraseña Incorrecta", "cerrar");
+            }
+        }
+
+        private void btnregistro_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new registro());
+        }
+    }
+}
---
+++ CineMovil/Vistas/registro.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CineMovil.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro()
+        {
+            InitializeComponent();
+        }
+
+        private void btncrear_Clicked(object sender, EventArgs e)
+        {
+            string nombre = Convert.ToString(txtnombre.Text);
+            string usuario = Convert.ToString(txtcorreo.Text);
+            string correo = Convert.ToString(txtcorreo.Text);
+            string cont = Convert.ToString(txtcontraseña.Text);
+
+            if(nombre == txtnombre.Text && usuario == txtusuario.Text)
+            {
+                DisplayAlert("Felicidades", "Nombre y usuario guardados con exito...", "cerrar");
+            }
+            if (correo == txtcorreo.Text && cont == txtcontraseña.Text)
+            {
+                DisplayAlert("Felicidades", "Correo y contraseña guardados con exito...", "cerrar");
+
+                Navigation.PushAsync(new login());
+            }
+        }
+    }
+}
